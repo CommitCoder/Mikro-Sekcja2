14f99e0245e669f9ff2799045d2ef602f26db65a
KubaKraken <k.jakub.dev.007@gmail.com>
2021-04-23 07:33:20 +0200

    9video my lesssons 1,2

--- src/main/java/com/firstApp/demo/DemoApplication.java
+++ src/main/java/com/firstApp/demo/DemoApplication.java
@@ -2,12 +2,38 @@ package com.firstApp.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
 
+	// spring context
+	ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+
+	//1 First lesson ##################################
+		// Dog visible when has @Component annotation
+		// SINGLETON Dog is created with instatiation  - sout() ..... Creating a dog
+//		Dog dog = context.getBean(Dog.class);
+//		dog.sayHello();
+//
+//		Dog dog2= context.getBean(Dog.class);
+//		dog2.sayHello();
+
+
+
+	// 2 Second lesson - bean is by default a singleton so dog and dog2 refers to same object
+	// - dog is created only once - constructor message ...
+
+//	Dog dog = context.getBean(Dog.class);
+//		dog.setName("Reks");
+//
+//	Dog dog2= context.getBean(Dog.class);
+//		System.out.println(dog2.getName()); // Reks
+
+
+		
+
+	}
 }
---
+++ src/main/java/com/firstApp/demo/Dog.java
@@ -0,0 +1,33 @@
+package com.firstApp.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog {
+    private Long id;
+    private String name;
+
+    public Dog() {
+        System.out.println("Creating a dog");
+    }
+
+    public void sayHello(){
+        System.out.println("Hello");
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
